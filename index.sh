eaa9dc9c42ff9479f04a7525e1b07a751c1f616f
luizx <ihaveluisprogra@gmail.com>
2020-04-08 16:01:53 -0300

    Criando e adicionando as infomacoes no projeto

---
+++ index.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+#---------------------------------------------#
+#  Pegainfo -> ele pega todos os dados do pc  #
+# title: PegaInfof                            #
+# autor: Luiszx                               #
+# versao: 0.1                                 #
+#---------------------------------------------#
